4f845ccb29a359f98a02178d446163b02be8e04c
jjvvll <llanetajohnver@gmail.com>
2026-04-13 16:22:48 +0800

    test

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    //
+    ///////
     public function login(Request $request)
     {
         $request->validate([
